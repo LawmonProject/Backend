5f5f4c89b88155442716610a00e07aea979638d7
frozzun <eonjun0783@naver.com>
2024-11-06 22:42:15 +0900

    LowmonBackend initial setting complete

---
+++ src/main/java/com/lawmon/lawmon/LawmonApplication.java
@@ -0,0 +1,13 @@
+package com.lawmon.lawmon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawmonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LawmonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lawmon/lawmon/LawmonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lawmon.lawmon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawmonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
