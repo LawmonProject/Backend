9b08b7ca41fbd1b3188005c1700c8f3c64774d6c
jongmine <jongmine.ch@gmail.com>
2024-11-13 14:12:11 +0900

    feat: Contract, Summary 엔티티 구현

--- src/main/java/com/lawmon/lawmon/Entity/Summary.java
+++ src/main/java/com/lawmon/lawmon/Entity/Summary.java
@@ -1,4 +1,24 @@
 package com.lawmon.lawmon.Entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Summary")
+@NoArgsConstructor
+@Getter
+@Setter
 public class Summary {
+
+    @Id
+    @Column(name = "summary_id", nullable = false, length = 255)
+    private String summaryId;
+
+    @Column(name = "message_id", nullable = false)
+    private String messageId;
+
+    @Column(name = "content", columnDefinition = "TEXT")
+    private String content;
 }
