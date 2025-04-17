3c8b6a4d9a503221021da37457206ec3615edb54
박보성 <147051376+Boseong0902@users.noreply.github.com>
2025-04-17 13:49:00 +0900

    Update SecurityConfig.java

--- src/main/java/com/lawmon/lawmon/config/SecurityConfig.java
+++ src/main/java/com/lawmon/lawmon/config/SecurityConfig.java
@@ -56,11 +56,14 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8080"));
+        configuration.setAllowedOrigins(List.of(
+            "http://localhost:5173",
+            "http://localhost:8000"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
-
+    
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
